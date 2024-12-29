b69334f7d7b0bb601d5cbcbf68c66c66bc472b4b
Dmitry Salyakin <d.salyakin@gmail.com>
2024-12-29 02:34:31 +0200

    Add FormData saving to LocalStorage

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,4 +1,59 @@
-const formData = {
+let formData = {
   email: '',
   message: '',
 };
+
+const feedbackFormEl = document.querySelector('.feedback-form');
+
+const fillFormFields = () => {
+  try {
+    if (localStorage.length === 0) {
+      return;
+    }
+
+    const formDataFromLS = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    );
+
+    formData = formDataFromLS;
+
+    for (const key in formDataFromLS) {
+      feedbackFormEl.elements[key].value = formDataFromLS[key];
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fillFormFields();
+
+const onFormFieldChange = event => {
+  const {
+    target: { name: fieldName, value: fieldValue },
+  } = event;
+
+  console.log(`${fieldName}: ${fieldValue}`);
+
+  formData[fieldName] = fieldValue.trim();
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+const onFeedbackFormSubmit = event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Please fill in all fields');
+    return;
+  }
+
+  console.log('Form data:', formData);
+
+  formData = { email: '', message: '' };
+
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+};
+
+feedbackFormEl.addEventListener('input', onFormFieldChange);
+feedbackFormEl.addEventListener('submit', onFeedbackFormSubmit);
